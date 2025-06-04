8e430fb375f175f89f6aecd49a32d995ed4225e5
Андрей Пряшников <prandruha2003@yandex.ru>
2025-06-04 11:50:31 +0300

    Revert "lw-8_fix"
    This reverts commit 1a43a0d9a14935d0bb0e8c83213ecc03c9485220.

--- lw-1/RankCalculator/Program.cs
+++ lw-1/RankCalculator/Program.cs
@@ -31,16 +31,16 @@ class Program
     {
         try
         {
-            var redis = await ConnectionMultiplexer.ConnectAsync(Environment.GetEnvironmentVariable("DB_MAIN"));
+            var redis = await ConnectionMultiplexer.ConnectAsync(Environment.GetEnvironmentVariable("DB_MAIN") ?? "redis-main:6379,password=1234");
             var db = redis.GetDatabase();
             
             var regionalRedisConnections = new Dictionary<string, IConnectionMultiplexer>();
             regionalRedisConnections["RU"] =
-                await ConnectionMultiplexer.ConnectAsync(Environment.GetEnvironmentVariable("DB_RU"));
+                await ConnectionMultiplexer.ConnectAsync(Environment.GetEnvironmentVariable("DB_RU") ?? "redis-ru:6379,password=1234");
             regionalRedisConnections["EU"] =
-                await ConnectionMultiplexer.ConnectAsync(Environment.GetEnvironmentVariable("DB_EU"));
+                await ConnectionMultiplexer.ConnectAsync(Environment.GetEnvironmentVariable("DB_EU") ?? "redis-eu:6379,password=1234");
             regionalRedisConnections["ASIA"] =
-                await ConnectionMultiplexer.ConnectAsync(Environment.GetEnvironmentVariable("DB_ASIA"));
+                await ConnectionMultiplexer.ConnectAsync(Environment.GetEnvironmentVariable("DB_ASIA") ?? "redis-asia:6379,password=1234");
 
             var regionalDbs = new Dictionary<string, IDatabase>();
             regionalDbs["RU"] = regionalRedisConnections["RU"].GetDatabase();
@@ -68,7 +68,7 @@ class Program
                 var regionValue = await db.StringGetAsync($"REGION-{id}");
                 
                 if (!regionValue.HasValue) return;
-
+                
                 var region = regionValue.ToString();
 
                 if (!regionalDbs.TryGetValue(region, out var regionalDb)) return;
@@ -112,3 +112,20 @@ class Program
         return Encoding.UTF8.GetBytes(eventJson);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
